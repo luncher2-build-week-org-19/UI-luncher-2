da1e9aadf6d2ae9f540a602a54a54219fecd1787
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-06 18:47:10 -0500

    Minor edits

--- js/login.js
+++ js/login.js
@@ -2,17 +2,21 @@ const login = document.querySelector('#login');
 console.log(login);
 
 login.addEventListener('click', () => {
-    document.location.href = 'school-dashboard.html';
+    document.location.href = 'school-dashboard.html'; // Directs to Admin dashboard
 })
 
 const registerBtn = document.querySelector('#registerBtn');
 
 registerBtn.addEventListener('click', ()  => {
-    document.location.href = 'school-homepage.html';
+    document.location.href = 'school-homepage.html'; // Directs to registration
 })
 
 const donorBtn = document.querySelector('#donorBtn');
 
 donorBtn.addEventListener('click', () => {
-    document.location.href = 'donor-homepage.html';
+    document.location.href = 'donor-homepage.html'; // Directs to donor page
 })
+
+// const formSubmit = document.querySelector('#formSubmit');
+//     formSubmit.submit();
+
