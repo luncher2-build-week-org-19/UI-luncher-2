4f58210ea4679c11fa338d7a07ed252c5f0e7793
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-14 03:58:44 -0500

    Added event listener for signup and donate button redirects to netlify

--- js/Main.js
+++ js/Main.js
@@ -1,19 +1,19 @@
 class Dropmenu {
     constructor (element) {
         this.element = element; 
-        console.log(this.element);
+        // console.log(this.element);
 
         this.button = this.element.querySelector('.dropmenu-btn');
-        console.log(this.button);
+        // console.log(this.button);
 
         this.content = this.element.querySelector('.dropmenu-content');
-        console.log(this.content);
+        // console.log(this.content);
 
         this.button.addEventListener('click', () => this.toggleContent());
     }
 
     toggleContent () {  //Toggles the menu button to show and hide links
-        console.log(this.button);
+        // console.log(this.button);
         this.content.classList.toggle('dropmenu-hidden');
     }
 }
@@ -25,3 +25,14 @@ class Dropmenu {
 
 //Selects the dropmenu class and cycles through all of the elements within the div
 const menu = document.querySelectorAll('.dropmenu').forEach(dropmenu => new Dropmenu (dropmenu));
+
+
+const signup = document.querySelectorAll('.cta-btn'); //Redirects SignUp and Donate buttons on homepage to Netlify
+// console.log(signup);
+
+signup.forEach(event => {
+    event.addEventListener('click', event => {
+        // console.log(`${event.type}`);
+        window.location.href = 'https://luncher2.netlify.com/';
+    });
+}) ;
